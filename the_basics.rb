515f386117150d2ec52109dc868fe2cc8b30ff77
ToniRib <tonimarierib@gmail.com>
2015-09-06 22:06:50 -0400

    Adds file for chapter 1: The Basics

---
+++ the_basics.rb
@@ -0,0 +1,40 @@
+# Exercises from chapter: The Basics
+
+# Exercise 1:
+"Toni " + "Rib"
+
+# Exercise 2:
+thousands = 1234 / 1000
+hundreds = 1234 % 1000 / 1000
+tens = 1234 % 100 / 10
+ones = 1234 % 10
+
+# Exercise 3:
+movies = {:fight_club => 1999, :limitless => 2011, :event_horizon => 1997, :dune => 1984}
+puts movies[:fight_club]
+puts movies[:limitless]
+puts movies[:event_horizon]
+puts movies[:dune]
+
+# Exercise 4:
+dates = [1999, 2011, 1997, 1984]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+
+# Exercise 5:
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# Exercise 6:
+puts 5.876 * 5.876
+puts 2.1 * 2.1
+puts 9.23 * 9.23
+
+# Exercise 7:
+# In an irb session, the error message says that the user supplied an incorrect syntax.
+# The irb interpreter was expecting a curly bracket '}' but the user put a parentheses
+# ')' instead.
